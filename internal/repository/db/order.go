87913c6e39fd6970daa5f52fb8674ee4484f60ca
oleg2210 <dmchnklg@gmail.com>
2026-01-25 08:27:06 +0500

    create fix

--- internal/repository/db/order.go
+++ internal/repository/db/order.go
@@ -3,13 +3,11 @@ package db
 import (
 	"context"
 	"database/sql"
-	"errors"
 	"fmt"
 	"strings"
 
 	domainerrors "github.com/Oleg2210/gophermart/internal/domain/domain_errors"
 	"github.com/Oleg2210/gophermart/internal/domain/entities"
-	"github.com/jackc/pgx/v5/pgconn"
 )
 
 type PgxOrderRepository struct {
@@ -17,12 +15,30 @@ type PgxOrderRepository struct {
 }
 
 func (r *PgxOrderRepository) Create(ctx context.Context, order entities.Order) error {
+	var existingUserID string
+	err := r.tx.tx.QueryRowContext(
+		ctx,
+		"SELECT user_id FROM orders WHERE id = $1",
+		order.ID,
+	).Scan(&existingUserID)
+
+	if err == nil {
+		if existingUserID == order.UserID {
+			return domainerrors.ErrOrderIDExists
+		}
+		return domainerrors.ErrOrderIDBelongsOther
+	}
+
+	if err != sql.ErrNoRows {
+		return err
+	}
+
 	query := `
         INSERT INTO orders(id, user_id, status, amount, created)
         VALUES ($1, $2, $3, $4, $5)
     `
 
-	_, err := r.tx.tx.ExecContext(ctx, query,
+	_, err = r.tx.tx.ExecContext(ctx, query,
 		order.ID,
 		order.UserID,
 		order.Status,
@@ -30,32 +46,7 @@ func (r *PgxOrderRepository) Create(ctx context.Context, order entities.Order) e
 		order.Created,
 	)
 
-	if err != nil {
-		var pgErr *pgconn.PgError
-		if errors.As(err, &pgErr) && pgErr.Code == "23505" {
-
-			var existingUserID string
-			err2 := r.tx.tx.QueryRowContext(
-				ctx,
-				"SELECT user_id FROM orders WHERE id = $1",
-				order.ID,
-			).Scan(&existingUserID)
-
-			if err2 != nil {
-				return domainerrors.ErrOrderIDExists
-			}
-
-			if existingUserID == order.UserID {
-				return domainerrors.ErrOrderIDExists
-			}
-
-			return domainerrors.ErrOrderIDBelongsOther
-		}
-
-		return err
-	}
-
-	return nil
+	return err
 }
 
 func (r *PgxOrderRepository) GetByID(ctx context.Context, orderID string) (entities.Order, error) {
